a4349375d773c623e7db8c1690b8e5607e457d55
hanzishuang <hanzishuang@feinno.com>
2018-08-24 20:46:52 +0800

    prg--proto创建脚本，解决无法生成问题

--- make_project.bat
+++ make_project.bat
@@ -1,10 +1,13 @@
+:: 先获取环境变量
+call tools\build\win\set_env.bat
+
 :: 构建Win下工程
 
 mkdir o_win
 cd o_win
 
 ::BOOST_ROOT
-
-cmake -G "Visual Studio 15 2017" ./../src/ -DINC_Protobuf=D:\Code\lib\protobuf\src -DLIB_Protobuf="D:\Code\lib\protobuf\o_win\Debug\libprotocd.lib"
+mkdir ..\src\lib\msg_proto\proto_class\
+cmake -G "Visual Studio 15 2017" ./../src/ -DTHRD_LIB_PATH=%im_lib_src%
 
 cd ..
