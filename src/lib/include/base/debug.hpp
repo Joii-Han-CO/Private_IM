3f20f367837a89c23470c46869dca8918be14bdd
hanzishuang <hanzishuang@feinno.com>
2018-08-29 20:07:05 +0800

    base--调试，可以同步在控制台输出

---
+++ src/lib/include/base/debug.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <string>
+#include <mutex>
+#include "log.hpp"
+
+#pragma region namespace
+namespace base {
+namespace debug {
+#pragma endregion
+
+#pragma region OutputInfo
+
+std::mutex g_output_sync_;
+
+template<typename ... T1>
+inline void OutPut(const char *sz, T1 ... args) {
+  std::unique_lock<std::mutex> lock(g_output_sync_);
+  auto d = base::FormatStr(sz, args...);
+  std::cout << d.c_str() << std::endl;
+}
+
+template<typename ... T1>
+inline void OutPut(const wchar_t *sz, T1 ... args) {
+  std::unique_lock<std::mutex> lock(g_output_sync_);
+  auto d = base::FormatStr(sz, args...);
+  std::cout << d.c_str() << std::endl;
+}
+
+#pragma endregion
+
+#pragma region namespace
+}
+}
+#pragma endregion
