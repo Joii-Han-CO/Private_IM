df05277db1d2089c008134fe410f7350268224b8
hanzishuang <hanzishuang@feinno.com>
2018-08-30 21:42:45 +0800

    msg--mqtt--完成消息模块

--- src/lib/msg_mqtt_client/mqtt_client_base.cpp
+++ src/lib/msg_mqtt_client/mqtt_client_base.cpp
@@ -41,43 +41,97 @@ CMqttClientBase::CMqttClientBase():
 
 CMqttClientBase::~CMqttClientBase() {}
 
-// 连接Mqtt服务器
+// 连接
 bool CMqttClientBase::Connect(const SMqttConnectInfo &info) {
   StartTask();
-
-  auto func = [this, info] () {
-    Mqtt_Connect(info);
-
-  };
-  AddTask(func);
+  AddTask(
+    std::bind(&CMqttClientBase::Mqtt_Connect, this, info));
   return true;
 }
 
-// 断开连接
-bool CMqttClientBase::Disconnect() {
+// 断开
+void CMqttClientBase::Disconnect() {
+  OnlineStatusChange(EMqttOnlineStatus::disconnecting);
   if (mqtt_ == nullptr) {
     PrintWarn("[Mqtt] not connected, mqtt_ == nullptr");
-    return true;
+    return;
   }
 
   mosquitto_destroy(mqtt_);
   mqtt_ = nullptr;
+  OnlineStatusChange(EMqttOnlineStatus::disconnected);
+  is_connected = false;
 
-  return true;
+  StopTask();
 }
 
 // 添加一个订阅
-bool CMqttClientBase::Subscribe() {
-  return false;
+bool CMqttClientBase::Subscribe(
+  const std::string &topic,
+  const std::function<void(std::vector<char>)> &func) {
+  if (map_sub_.find(topic) != map_sub_.end()) {
+    SetLastErrAndLog("[Mqtt] subscribe %s failed, duplicate", topic.c_str());
+    return false;
+  }
+  map_sub_.insert(
+    std::pair<std::string, std::function<void(const std::vector<char>&)>>(
+      topic, func));
+
+  auto ref = mosquitto_subscribe(mqtt_, nullptr, topic.c_str(), 1);
+  if (ref != MOSQ_ERR_SUCCESS) {
+    SetLastErrAndLog("[Mqtt] subscribe %s failed, code: %d",
+                     topic.c_str(), ref);
+    return false;
+  }
+  return true;
 }
 
 // 推送消息
-bool CMqttClientBase::Publish() {
-  return false;
+bool CMqttClientBase::Publish(const std::string &topic,
+                              const std::vector<char> &data,
+                              const std::function<void()> &func) {
+  int pub_id = publish_count_id_++;
+  map_pub_.insert(std::pair<int, std::function<void()>>(pub_id, func));
+  int ref = mosquitto_publish(mqtt_, nullptr, topic.c_str(),
+                              data.size(), data.data(),
+                              pub_id, true);
+  if (ref != MOSQ_ERR_SUCCESS) {
+    SetLastErrAndLog("[Mqtt] publish %s failed, data_size:%d, code: %d",
+                     topic.c_str(), data.size(), ref);
+    map_pub_.erase(pub_id);
+    return false;
+  }
+  return true;
+}
+
+// 格式化在线状态
+std::string CMqttClientBase::FormatOnlineStatusA(EMqttOnlineStatus s) {
+  switch (s) {
+  case im::EMqttOnlineStatus::none:
+    return "none";
+  case im::EMqttOnlineStatus::connecting:
+    return "connecting";
+  case im::EMqttOnlineStatus::connected:
+    return "connected";
+  case im::EMqttOnlineStatus::disconnecting:
+    return "disconnecting";
+  case im::EMqttOnlineStatus::disconnected:
+    return "disconnected";
+  default:
+    break;
+  }
+  return "unknow";
 }
 
 // mqtt同步连接
 void CMqttClientBase::Mqtt_Connect(const SMqttConnectInfo &info) {
+  if (is_connected == true) {
+    SetLastErrAndLog("[Mqtt] is connected");
+    return;
+  }
+
+  OnlineStatusChange(EMqttOnlineStatus::connecting);
+
   auto ref = GlobalInitMqtt::Get()->Init();
   if (ref != MOSQ_ERR_SUCCESS) {
     SetLastErrAndLog("[Mqtt] global init failed, code: %d", ref);
@@ -105,6 +159,13 @@ void CMqttClientBase::Mqtt_Connect(const SMqttConnectInfo &info) {
     return;
   }
   is_connected = true;
+  OnlineStatusChange(EMqttOnlineStatus::connected);
+
+  loop_timeout_ = info.loop_timeout;
+
+  // 下个消息循环，进入循环
+  AddTask(
+    std::bind(&CMqttClientBase::Mqtt_MsgLoop, this));
 }
 
 // mqtt连接前，设置一些相关参数
@@ -116,7 +177,82 @@ bool CMqttClientBase::Mqtt_InitOpts(const SMqttConnectInfo &info) {
     return false;
   }
 
+  mosquitto_message_callback_set(mqtt_, SMsg_Cb);
+  mosquitto_publish_callback_set(mqtt_, SPub_Cb);
+
   return true;
+}
+
+// 消息循环
+void CMqttClientBase::Mqtt_MsgLoop() {
+  while (true) {
+    int ref = mosquitto_loop(mqtt_, loop_timeout_, 1);
+    if (ref == 0) {
+
+    }
+  }
+}
+
+// 消息循环的静态回调
+void CMqttClientBase::SMsg_Cb(mosquitto *mosq, void *obj,
+                              const mosquitto_message *message) {
+  if (obj == nullptr)
+    return;
+  CMqttClientBase *this_ = (CMqttClientBase*)obj;
+  if (mosq == nullptr || mosq != this_->mqtt_)
+    return;
+  this_->Msg_Cb(message);
+}
+void CMqttClientBase::Msg_Cb(const mosquitto_message *message) {
+  if (message == nullptr) {
+    PrintWarn("[Mqtt] message failed, message is null");
+    return;
+  }
+  if (message->topic == nullptr || message->topic[0] == '\0') {
+    PrintWarn("[Mqtt] message failed, topic is null");
+    return;
+  }
+  if (message->payloadlen <= 0) {
+    PrintWarn("[Mqtt] message failed, data size is 0");
+    return;
+  }
+  if (message->payload == nullptr) {
+    PrintWarn("[Mqtt] message failed, payload is null");
+    return;
+  }
+
+  std::string topic = message->topic;
+  std::vector<char> data;
+  data.resize(message->payloadlen);
+  memcpy(data.data(), message->payload, data.size());
+
+  auto it = map_sub_.find(topic);
+  if (it == map_sub_.end()) {
+    PrintWarn(
+      "[Mqtt] message failed, No corresponding callback found");
+    return;
+  }
+  if (it->second)
+    it->second(data);
+}
+
+void CMqttClientBase::SPub_Cb(mosquitto *mosq, void *obj, int data) {
+  if (obj == nullptr)
+    return;
+  CMqttClientBase *this_ = (CMqttClientBase*)obj;
+  if (mosq == nullptr || mosq != this_->mqtt_)
+    return;
+  this_->Pub_Cb(data);
+}
+void CMqttClientBase::Pub_Cb(int data) {
+  auto it = map_pub_.find(data);
+  if (it == map_pub_.end()) {
+    PrintWarn("[Mqtt] publish failed, No corresponding callback found");
+    return;
+  }
+  if (it->second)
+    it->second();
+  map_pub_.erase(data);
 }
 
 #pragma region
--- src/lib/msg_mqtt_client/mqtt_client_base.h
+++ src/lib/msg_mqtt_client/mqtt_client_base.h
@@ -14,7 +14,15 @@ struct SMqttConnectInfo {
   int port = 1883;
   int keepalive = 30;
   int max_inflight_msg = -1;  // 如果离线，最大消息数。默认-1（mosquitto 默认为20）
+  int loop_timeout = 0;   // 每次消息循环的超时时间
+};
 
+enum class EMqttOnlineStatus {
+  none,   // 最初状态
+  connecting, // 正在连接
+  connected,  // 连接成功
+  disconnecting, // 正在断开
+  disconnected,   // 已断开
 };
 
 class CMqttClientBase:
@@ -26,23 +34,25 @@ public:
   CMqttClientBase();
   ~CMqttClientBase();
 
-  // 描述：连接Mqtt服务器
+  // 描述：连接\断开
+  //  对外接口，异步执行
   bool Connect(const SMqttConnectInfo &info);
-
-  // 描述：断开连接
-  bool Disconnect();
+  void Disconnect();
 
   // 描述：添加一个订阅
-  bool Subscribe();
+  bool Subscribe(const std::string &topic,
+                 const std::function<void(std::vector<char>)> &func);
 
   // 描述：推送消息
-  bool Publish();
+  bool Publish(const std::string &topic,
+               const std::vector<char> &data,
+               const std::function<void()> &func);
+
+  static std::string FormatOnlineStatusA(EMqttOnlineStatus s);
 
 protected:
-  virtual void Connected() = 0;
-  virtual void Subscribed() = 0;
-  virtual void Published() = 0;
-  virtual void Messaged(const mosquitto_message *msg) = 0;
+  virtual void OnlineStatusChange(EMqttOnlineStatus status) = 0;
+  virtual void Subscribed(const std::string &topic) = 0;
   virtual void OutLog(const base::log::SBaseLog &func) = 0;
   VirtualPrintLogFunc(OutLog);
 
@@ -53,13 +63,24 @@ private:
   // mqtt连接前，设置一些相关参数
   bool Mqtt_InitOpts(const SMqttConnectInfo &info);
 
+  void Mqtt_MsgLoop();
+
+  static void SMsg_Cb(mosquitto *mosq, void *obj,
+                      const mosquitto_message *message);
+  void Msg_Cb(const mosquitto_message *message);
+
+  static void SPub_Cb(mosquitto *mosq, void *obj, int data);
+  void Pub_Cb(int data);
+
 private:
   mosquitto * mqtt_;
-
   bool is_connected = false;
+  int loop_timeout_ = 0;
 
-  // 各种默认常量
+  std::map<std::string, std::function<void(const std::vector<char>&)>> map_sub_;
 
+  int publish_count_id_ = 0;
+  std::map<int, std::function<void()>> map_pub_;
 };
 typedef std::shared_ptr<CMqttClientBase> pCMqttClientBase;
 
