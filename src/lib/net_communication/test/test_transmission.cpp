3363e80088d55c54d71d1c7faf3654ce3eb82f79
hanzishuang <hanzishuang@feinno.com>
2018-08-31 20:07:40 +0800

    net--communication--添加测试用CPP文件

--- src/lib/net_communication/test/test_transmission.cpp
+++ src/lib/net_communication/test/test_transmission.cpp
@@ -1,5 +1,5 @@
-﻿#pragma once
-#include <iostream>
+﻿#include "stdafx.h"
+#include "test_net_communication.h"
 
 #pragma region namespace
 namespace test {
@@ -8,3 +8,4 @@ namespace test {
 #pragma region namespace
 }
 #pragma endregion
+
