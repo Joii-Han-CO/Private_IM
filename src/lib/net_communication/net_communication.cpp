7427decab4c8da0e3478a052b1ab8fe8ba1ba58f
hanzishuang <hanzishuang@feinno.com>
2018-09-01 15:39:21 +0800

    net--communication--接口

---
+++ src/lib/net_communication/net_communication.cpp
@@ -0,0 +1,43 @@
+﻿#include "stdafx.h"
+#include "net_communication.h"
+
+
+#pragma region namespace
+namespace im {
+namespace nc {
+#pragma endregion
+
+CNetCom::CNetCom():
+  base::log::Log(std::bind(&CNetCom::LogCallabck,
+                           this, std::placeholders::_1)) {}
+
+CNetCom::~CNetCom() {}
+
+bool CNetCom::Init(const SNetCom_InitArgs &args,
+                   const std::function<void(int)> &cb) {
+  return true;
+}
+
+bool CNetCom::Release(const std::function<void()> &cb) {
+  return true;
+}
+
+bool CNetCom::Send(std::vector<char> data,
+                   const FUNC_SendCallback &cb) {
+  return true;
+}
+
+bool CNetCom::Request(std::vector<char> data,
+                      const FUNC_RequestCallback &cb) {
+  return true;
+}
+
+void CNetCom::LogCallabck(const base::log::SBaseLog &func) {
+  if (cb_log_)
+    cb_log_(func);
+}
+
+#pragma region namespace
+}
+}
+#pragma endregion
---
+++ src/lib/net_communication/net_communication.h
@@ -0,0 +1,72 @@
+﻿#pragma once
+#include "base/task.hpp"
+#include "base/log.hpp"
+#include "base/error.hpp"
+#include <functional>
+#include <vector>
+#include <memory>
+
+
+#pragma region namespace
+namespace im {
+namespace nc {
+#pragma endregion
+
+typedef std::function<void()> FUNC_MessageCallback;
+typedef std::function<void(const base::log::SBaseLog &func)> FUNC_LogCallback;
+
+struct SNetCom_InitArgs {
+  std::string host;
+  int port = -1;   // 如果为 -1 则自动查找到一个默认端口使用
+  bool listener = false;
+
+  FUNC_MessageCallback cb_message;
+  FUNC_LogCallback cb_log;
+};
+
+enum class ENetCom_ReqStatus {
+  none,
+};
+
+class CNetCom:
+  private base::task::Task,
+  private base::log::Log,
+  public base::error::LastError {
+public:
+  CNetCom();
+  virtual ~CNetCom();
+
+  // 描述：初始化，异步
+  //  cb  初始化完成后的回调
+  //        参数1   打开的端口号
+  bool Init(const SNetCom_InitArgs &args,
+            const std::function<void(int)> &cb);
+
+  // 描述：释放资源，异步
+  //  cb    完成后的通知
+  bool Release(const std::function<void()> &cb);
+
+  // 描述：发送消息
+  typedef std::function<void(SNetCom_InitArgs)> FUNC_SendCallback;
+  bool Send(std::vector<char> data,
+            const FUNC_SendCallback &cb);
+
+  // 描述：请求数据
+  typedef std::function<void(ENetCom_ReqStatus, std::vector<char>)>
+    FUNC_RequestCallback;
+  bool Request(std::vector<char> data,
+               const FUNC_RequestCallback &cb);
+
+private:
+  void LogCallabck(const base::log::SBaseLog &func);
+
+private:
+  FUNC_LogCallback cb_log_ = nullptr;
+
+};
+typedef std::shared_ptr<CNetCom> pCNetCom;
+
+#pragma region namespace
+}
+}
+#pragma endregion
