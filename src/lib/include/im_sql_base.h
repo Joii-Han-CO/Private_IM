8bb8a3e0e47ceb2f887fb9dee3511fb345832cd1
hanzishuang <hanzishuang@feinno.com>
2018-09-19 10:17:28 +0800

    sql--sql_base--CSqlBase class

--- src/lib/include/im_sql_base.h
+++ src/lib/include/im_sql_base.h
@@ -1,5 +1,8 @@
 ﻿#pragma once
 #include "base/type_def.h"
+#include "base/error.hpp"
+#include "base/log.hpp"
+#include "base/task.hpp"
 
 
 #pragma region
@@ -7,6 +10,20 @@ namespace im {
 namespace sql_base {
 #pragma endregion
 
+// 描述：主要为了处理Sql的基类
+//  错误、日志的处理
+//  添加任务
+
+class CSqlBase:
+  public base::error::LastError,
+  public base::log::Log,
+  public base::task::Task {
+
+public:
+  CSqlBase(base::log::LogCallback log_func);
+  ~CSqlBase();
+
+};
 
 #pragma region
 }
--- src/lib/sql_base/im_sql_base.cpp
+++ src/lib/sql_base/im_sql_base.cpp
@@ -6,7 +6,16 @@ namespace im {
 namespace sql_base {
 #pragma endregion
 
-  
+CSqlBase::CSqlBase(base::log::LogCallback log_func):
+  Log(log_func) {
+  StartTask();
+}
+
+CSqlBase::~CSqlBase() {
+  StopTask();
+  WaitTask();
+}
+
 #pragma region namespace
 }
 }
