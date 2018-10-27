6fcf7198cdc2bce2eda17c3eb17da5267608d50e
joii <hanzishuang1990@126.com>
2018-10-27 11:45:51 +0800

    plugin_py--create project

---
+++ src/plugin/plugin_py/im_plugin_py.cpp
@@ -0,0 +1,16 @@
+﻿#include "stdafx.h"
+#include "im_plugin_py.h"
+
+#pragma region namespace
+namespace im {
+namespace plugin_py {
+#pragma endregion
+
+  bool Test_plugin_py_C(IN int test_args) {
+    return true;
+  }
+  
+#pragma region namespace
+}
+}
+#pragma endregion
---
+++ src/plugin/plugin_py/test/test_plugin_py.cpp
@@ -0,0 +1,23 @@
+﻿#include "stdafx.h"
+#include "test_plugin_py.h"
+#include "im_plugin_py.h"
+#pragma region namespace
+namespace test {
+#pragma endregion
+
+  void test_C(){
+    std::cout << "test_plugin_py_C Begin:" << std::endl;
+    im::plugin_py::Test_plugin_py_C(8);
+    std::cout << "test_plugin_py_C Begin:" << std::endl;
+  }
+
+#pragma region namespace
+}
+#pragma endregion
+
+int main() {
+  
+  test::test_C();
+
+  return 0;
+}
