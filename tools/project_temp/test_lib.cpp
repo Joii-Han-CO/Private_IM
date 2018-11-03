ba5b07ea293dd46d513890b05cf69740b412cdc8
joii <hanzishuang1990@126.com>
2018-11-03 16:01:23 +0800

    Script--Template--CMake and del .hpp file

--- tools/project_temp/test_lib.cpp
+++ tools/project_temp/test_lib.cpp
@@ -1,7 +1,6 @@
 ﻿#include "stdafx.h"
 #include "test_$(--prj_name--).h"
 #include "im_$(--prj_name--).h"
-#include "im_$(--prj_name--).hpp"
 
 #pragma region namespace
 namespace test {
@@ -13,12 +12,6 @@ namespace test {
     std::cout << "test_$(--prj_name--)_C Begin:" << std::endl;
   }
 
-  void test_CPP(){
-    std::cout << "test_$(--prj_name--)_CPP Begin:" << std::endl;
-    im::$(--prj_name--)::Test_$(--prj_name--)_CPP(8);
-    std::cout << "test_$(--prj_name--)_CPP Begin:" << std::endl;
-  }
-
 #pragma region namespace
 }
 #pragma endregion
@@ -26,7 +19,6 @@ namespace test {
 int main() {
   
   test::test_C();
-  test::test_CPP();
 
   return 0;
 }
